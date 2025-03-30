61feb0ee9682b7e2f41f03769bb65240ba5de443
Eduardo Gonelli <37277530+Eduardo-Gonelli@users.noreply.github.com>
2025-03-30 12:32:00 -0300

    Update
    Incluído Shuffle utilizando System.Linq na cena Aleatoriedade e incluída cena TransiçãoEstados

--- Assets/Scripts/TransicaoEstado.cs
+++ Assets/Scripts/TransicaoEstado.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using System.Linq;
+using TMPro;
 
 public class TransicaoEstado : MonoBehaviour
 {
+    public TextMeshProUGUI txtRandom;
+    public TextMeshProUGUI txtCurrentState;
+    public TextMeshProUGUI txtNumberOfStates;
     // Cria uma lista de estados possíveis
     enum Estado { Atacar, Fugir, SeEsconder, Patrulhar }
     // Cria uma variável para armazenar o estado atual
@@ -19,22 +24,43 @@ public class TransicaoEstado : MonoBehaviour
             else { tabela[i] = (int)Estado.Patrulhar; }
         }
         // Faz um shuffle no array para que os estados sejam aleatorizados
-        for (int i = 0; i < 100; i++)
-        {
-            int temp = tabela[i];
-            int randomIndex = Random.Range(i, 99);
-            tabela[i] = tabela[randomIndex];
-            tabela[randomIndex] = temp;
-        }
+        tabela = ShuffleArray(tabela);
+        // tabela = KnuthShuffle(tabela);
 
-        // Faz um sorteio para definir o novo estado
-        int novoEstado = Random.Range(0, 99);
+        PrintCurrentState();
+        PrintNumberOfStates();
+        PrintAllStates();
+    }
+
+    // Exemplo de método para embaralhar um array usando System.Linq, por Thomas Steffen
+    // Fonte: https://thomassteffen.medium.com/super-simple-array-shuffle-with-linq-167b317ba035
+    int[] ShuffleArray(int[] array)
+    {
+        System.Random random = new System.Random();
+        return array.OrderBy(x => random.Next()).ToArray();
+    }
+
+    // Algoritmo Knuth Shuffle para aleatorização de arrays (alternativa ao ShuffleArray com System.Linq)
+    // Fonte: https://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+    int[] KnuthShuffle(int[] array)
+    {
+        for (int t = 0; t < array.Length; t++)
+        {
+            int tmp = array[t];
+            int r = Random.Range(t, array.Length);
+            array[t] = array[r];
+            array[r] = tmp;
+        }
+        return array;
+    }
+
+    void PrintCurrentState()
+    {
+        // Faz um sorteio de 0 a 99 para definir o estado atual
+        int novoEstado = Random.Range(0, 100);
         // Atualiza o estado atual
         estadoAtual = (Estado)tabela[novoEstado];
-        Debug.Log("Estado Atual: " + estadoAtual);
-
-        //PrintNumberOfStates();
-        PrintAllStates();
+        txtCurrentState.text = "Estado Atual: " + estadoAtual;
     }
 
     // Método para imprimir o número de estados
@@ -45,6 +71,8 @@ public class TransicaoEstado : MonoBehaviour
         int counterState2 = 0;
         int counterState3 = 0;
 
+        // Aqui fazemos um cast do nome do Estado para um int para comparar com i
+        // Se o valor de i for igual ao valor do Estado, incrementamos o respectivo contador
         foreach (int i in tabela)
         {
             if (i == (int)Estado.Atacar) counterState0++;
@@ -52,21 +80,27 @@ public class TransicaoEstado : MonoBehaviour
             if (i == (int)Estado.SeEsconder) counterState2++;
             if (i == (int)Estado.Patrulhar) counterState3++;
         }
-        Debug.Log(
-            "Número de estados:\n"+
+        txtNumberOfStates.text = 
+            "Total de Estados:\n"+
             "Atacar: " + counterState0 + "\n" +
             "Fugir: " + counterState1 + "\n" +
             "Se Esconder: " + counterState2 + "\n" +
             "Patrulhar: " + counterState3
-            );
+            ;
     }
 
     // Método para imprimir todos os estados
     void PrintAllStates()
     {
+        txtRandom.text = "Estados Embaralhados:\n";
         foreach (int i in tabela)
         {
-            Debug.Log((Estado)i);
+            // Estado é um enum e quando faz um cast do int para Estado, ele retorna o nome do estado
+            txtRandom.text += (Estado)i + ", ";
         }
+        // Remove a vírgula e o espaço do último estado
+        txtRandom.text = txtRandom.text.Substring(0, txtRandom.text.Length - 2);
+        // Adiciona o ponto final no último estado
+        txtRandom.text += ".";
     }
 }
